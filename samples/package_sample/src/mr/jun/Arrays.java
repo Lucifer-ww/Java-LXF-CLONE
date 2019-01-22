d8dec34b809e42dd6013e70669255e97e892727b
Michael Liao <askxuefeng@gmail.com>
2019-01-22 12:36:30 +0800

    add package sample

---
+++ samples/package_sample/src/mr/jun/Arrays.java
@@ -0,0 +1,10 @@
+package mr.jun;
+
+import ming.Person;
+
+public class Arrays {
+    public static void main(String[] args) {
+        Person p = new Person();
+        System.out.println("Run mr.jun.Arrays.main()");
+    }
+}
